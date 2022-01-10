f2d5485f855dfb21c1ade38a063676eb2e5f205e
andersoww <andersonbarrinha18@gmail.com>
2022-01-10 20:15:56 -0300

    Front-end/Pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Button from './components/Button';
+import Header from './components/Header';
+import ListPokeCard from './components/ListPokeCard';
+import Loader from './components/Loader';
+import { FetchData, GetURL } from './services/api';
+import './styles/global.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Data {
+    next: string;
+    previous: string;
+    results: [];
+}
+interface ResultsData {
+    name: string;
+    url: string;
 }
 
+function App() {
+    //Hook para Guardar os Pokémons
+    const [AllPokemon, setAllPokemon] = useState([])
+    const [next, setNext] = useState('');
+    const [previous, setPrevious] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const fecthAPI = async () => {
+        let data: Data = await FetchData();
+        setNext(data.next)
+        setPrevious(data.previous)
+        await GetAllPokemon(data.results)
+
+    }
+    const GetAllPokemon = async (array: []) => {
+        let _pokedata: any = await Promise.all(array.map(async (item: ResultsData) => {
+            let dataurl = await GetURL(item.url)
+            return dataurl;
+        }))
+        setAllPokemon(_pokedata)
+        setLoading(true);
+    }
+    useEffect(() => {
+        fecthAPI()
+    }, [])
+    const handleNext = async () => {
+        setLoading(false)
+        const response: Data = await FetchData(next)
+        await GetAllPokemon(response.results)
+        setNext(response.next);
+        setPrevious(response.previous);
+    }
+    const handlePrevious = async () => {
+        if (previous) {
+            setLoading(false)
+            const response: Data = await FetchData(previous)
+            await GetAllPokemon(response.results)
+            setNext(response.next);
+            setPrevious(response.previous);
+        }
+    }
+    return (
+        <div className="App">
+            <Header />
+            <Button next={handleNext} previous={handlePrevious} />
+            {!loading ? <Loader /> : <ListPokeCard data={AllPokemon} />}
+        </div>
+    );
+}
 export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+interface Button {
+
+}
+const Button = ({ next, previous }: any) => {
+    return (
+        <div className='container-btn'>
+            <button className='btn' onClick={previous}>Anterior</button>
+            <button className='btn' onClick={next} >Próximo</button>
+        </div>
+    );
+}
+export default Button;
